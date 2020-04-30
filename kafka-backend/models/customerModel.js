91502fe04bcacf975fe2db9c33a051a92394e406
deepkhajanchi <45371064+deepkhajanchi@users.noreply.github.com>
2020-04-30 11:49:24 -0700

    changed ImageName in models

--- kafka-backend/models/customerModel.js
+++ kafka-backend/models/customerModel.js
@@ -1,12 +1,51 @@
 const mongoose = require('mongoose');
+const Product =require('./productModel');
+const Review = require('./reviewModel');
 
 const Schema = mongoose.Schema;
+var addressSchema = new Schema({
+    Street: {type: String, required: true},
+    Apt: {type: String, required: false},
+    City: {type: String, required: true},
+    State: {type: String, required: true},
+    Zipcode: {type: String, required: true},
+    Country: {type: String, required: true},
+});
+
+var paymentSchema = new Schema({
+    Number: {type: String, required: true},
+    NameOnCard: {type: String, required: true},
+    ExpDate: {type: Date, required: true},
+});
+
+var saveForLater = new Schema({
+    ProductID: { type: Schema.Types.ObjectId, ref: Product },
+    Quantity: {type: Number, required: true},
+    Price: {type: Number, required:true},
+    IsGift: {type: Boolean, required:true},
+    GiftMessage: {type: String, required: false},
+});
+
+var cartSchema = new Schema({
+    ProductID: { type: Schema.Types.ObjectId, ref: Product },
+    Quantity: {type: Number, required: true},
+    Price: {type: Number, required:true},
+    IsGift: {type: Boolean, required:true},
+    GiftMessage: {type: String, required: false},
+});
 
 var customerSchema = new Schema({
-    name: String,
-    email: String,
-    password: String,
-
+    Name:  {type: String, required: true},
+    Email:  {type: String, required: true},
+    Password:  {type: String, required: true},
+    ImageName: {type:String, default: "none", required: false},
+    ImageData: {type: String, required: false},
+    Address: [{type: addressSchema}],
+    Payments: [{type: paymentSchema}],
+    Cart: [{type: cartSchema}],
+    SaveForLater:[{type : saveForLater}],
+    CommentsRef: [{type: String}],
+    
 },
     {
         versionKey: false
