87bab06865bc8fed859295e6cf1b095439729275
deepkhajanchi <45371064+deepkhajanchi@users.noreply.github.com>
2020-04-30 11:50:02 -0700

    changed ImageName in models

--- kafka-backend/models/sellerModel.js
+++ kafka-backend/models/sellerModel.js
@@ -2,11 +2,19 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-var sellerSchema = new Schema({
-    name: String,
-    email: String,
-    password: String,
+var addressSchema = new Schema({
+   c
+});
 
+var sellerSchema = new Schema({
+    Name: {type: String, required: true},
+    Email:  {type: String, required: true},
+    Password:  {type: String, required: true},
+    ImageName:  {type: String, default: "none", required: false},
+    ImageData:{type: String, required: false},
+    Address: addressSchema,
+    Products: [{type: String}],
+    Description:{type: String, required: false},
 },
     {
         versionKey: false
