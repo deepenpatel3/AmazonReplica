c09285e02a94026903cedd8e6bc116c382b34611
deepkhajanchi <45371064+deepkhajanchi@users.noreply.github.com>
2020-04-30 11:48:41 -0700

    Update adminModel.js

--- kafka-backend/models/adminModel.js
+++ kafka-backend/models/adminModel.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 var adminSchema = new Schema({
-    name: String,
-    email: String,
-    password: String,
-
+    Name: {type: String, required: true},
+    Email: {type: String, required: true},
+    Password: {type: String, required: true},
+    ProfileURL: {type: String, required: false},
 },
     {
         versionKey: false
