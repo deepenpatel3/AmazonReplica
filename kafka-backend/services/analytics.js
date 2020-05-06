b4e7181caf4bb13105f13bf040786fd8f1e45cb9
acs12 <46435796+acs12@users.noreply.github.com>
2020-05-06 03:15:48 -0700

    Admin Analytics Order Per Day Implementation

--- kafka-backend/services/analytics.js
+++ kafka-backend/services/analytics.js
@@ -11,6 +11,9 @@ exports.serve = function serve(msg, callback) {
         case "top_10_sellers":
             top_10_sellers(msg.body, callback);
             break;
+        case "orders_per_day":
+            orders_per_day(msg, callback)
+            break
     }
 }
 
@@ -39,4 +42,38 @@ function top_10_sellers(msg, callback) {
             callback(null, result);
         }
     })
+}
+
+
+async function orders_per_day(msg, callback) {
+    let Counts = []
+    const promise = new Promise((resolve,reject) =>{
+
+        for (let i = 0; i < 7; i++) {
+        let date = new Date(+new Date() - i * 24 * 60 * 60 * 1000).toISOString().slice(0, 19).replace('T', ' ');
+        date = date.slice(0, 11)
+        console.log("Date", date)
+        let query = " SELECT COUNT(Order_id) as orders  FROM `Order` where OrderDate = '" + date + "' ";
+        mysql.executeQuery(query, async function (err, result) {
+            if (err) {
+                console.log("error ", err);
+                callback(err, null);
+            } else {
+                console.log("Orders per day Result", result[0].orders)
+                // callback(null, result);
+                await Counts.push({ "Date": date, "Count": result[0].orders })
+                console.log("Counts", Counts)
+            }
+        })
+    }
+    
+    setTimeout( function() {
+        resolve("Success!")  // Yay! Everything went well!
+      }, 3000)
+})
+    promise.then( result=>{
+        console.log("In promise")
+        callback(null, Counts)
+    })
+
 }
