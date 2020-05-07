e80eed263c7981341ad8caf6a81ffc1b41361476
Deepen Patel <dipenkpatel3@gmail.com>
2020-05-06 19:31:14 -0700

    added admin category apis and category model

---
+++ backend/src/routes/admin/category.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const router = express.Router();
+const kafka = require("../../../kafka/client");
+
+router.post("/addCategory", (req, res) => {
+
+    kafka.make_request('product', { "path": "add_category", "body": req.body }, function (err, result) {
+        console.log("got back from add_category kafka");
+        if (!result) {
+            console.log("err ", err);
+            res.end();
+        } else {
+            console.log("result ", result);
+            res.status(200);
+            res.end(result.toString());
+        }
+    });
+
+})
+
+router.post("/removeCategory", (req, res) => {
+
+    kafka.make_request('product', { "path": "remove_category", "body": req.body }, function (err, result) {
+        console.log("got back from add_category kafka");
+        if (!result) {
+            console.log("err ", err);
+            res.end();
+        } else {
+            console.log("result ", result);
+            res.status(200);
+            res.json(result);
+            res.end();
+        }
+    });
+
+})
+
+router.get("/getProducts", (req, res) => {
+
+    kafka.make_request('product', { "path": "get_category_products", "body": req.query }, function (err, result) {
+        console.log("got back from get_category_products kafka");
+        if (!result) {
+            console.log("err ", err);
+            res.end();
+        } else {
+            // console.log("result ", result);
+            res.status(200);
+            res.json(result);
+            res.end();
+        }
+    });
+})
+module.exports = router;
